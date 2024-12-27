019fb47dc5213204adee018c7fb7cc88db7b3eec
Łukasz Michalak <lupinummail@gmail.com>
2024-12-27 19:47:51 +0100

    init commit

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import threading
 import tkinter as tk
 from PIL import Image, ImageTk
 import cv2
+import time
 
 image_counter = 0
 file_name = ""
 image = None
 camera_view = None
+
+# ================================ TO DO ================================
+#   [X] Dodać migającą kropkę na obrazie z kamery
+#   [ ] Pobrać informacje o nazwie kamery
+#   [ ] Resetowanie kamery
+
+lastUpdate = int(time.time())
 
 class CameraThread(threading.Thread):
     def __init__(self, root, camera_view, camera_index=0):
@@ -20,9 +28,17 @@ class CameraThread(threading.Thread):
     def run(self):
         while self.running:
             ret, frame = self.capture.read()
+            global lastUpdate
 
             if not ret:
                 print("Failed to grab frame from camera.")
+
+                image = Image.open(default_image_path).resize((400, 300))
+                image = ImageTk.PhotoImage(image)
+
+                camera_view = tk.Label(root, image=image)
+                camera_view.configure(image=image)
+                camera_view.image = image
                 break
 
             # Zmiana rozmiaru o 50% (zmniejszenie)
@@ -32,6 +48,13 @@ class CameraThread(threading.Thread):
             dim = (width, height)
 
             frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+
+            if int(time.time()) - lastUpdate < 1:
+	            frame = cv2.circle(frame, (20, 20), radius=10, color=(0, 0, 255), thickness=-1)
+            elif int(time.time()) - lastUpdate >= 2:
+	            lastUpdate = int(time.time())
+
+            frame = cv2.putText(frame, "iPhone CAM", (40, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
             # Convert the OpenCV image to ImageTk format
             image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert colors
@@ -55,6 +78,7 @@ def save_image():
     print("Saving image: {}_{}.png".format(file_name, image_counter))
     image_counter += 1
 
+# ================================ Threads ================================
 def save_image_thread():
 	global counterEntryText
 	counterEntryText.set(str(image_counter + 1))
@@ -64,7 +88,13 @@ def bookEntryChanged(*args):
 	global file_name
 	file_name = bookEntryText.get()
 
-# GUI
+def start_camera_thread():
+	print("Starting the camera thread...")
+	camera_thread = CameraThread(root, camera_view)
+	camera_thread.start()
+	return camera_thread
+
+# ================================ GUI ================================
 root = tk.Tk()
 root.title("Book Scan By Kitku Soft")
 
@@ -98,17 +128,22 @@ camera_view.image = image
 
 camera_view.grid(row=0, column=0, rowspan = 5)
 
-
 # Save Button
 savePictureButton = tk.Button(root, text="Save picture", command=save_image_thread)
 savePictureButton.grid(row=4, column=2, columnspan=2, pady=2)
+
+# Reset camera
+resetCameraButton = tk.Button(root, text="Reset camera", command=start_camera_thread)
+resetCameraButton.grid(row=4, column=3, columnspan=2, pady=2)
 
 # OpenCV
 camera_thread = CameraThread(root, camera_view)
 camera_thread.start()
 
+# main loop
 tk.mainloop()
 
+# ================================ Clean up ================================
 print("Closing the camera thread...")
 camera_thread.stop()
 
